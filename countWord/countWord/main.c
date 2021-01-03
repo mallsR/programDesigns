b5e25c9d366301d53dd9e47541214b634b9769a1
mallsR <2471385107@qq.com>
2021-01-03 18:58:04 +0800

    first

---
+++ countWord/countWord/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  countWord
+//
+//  Created by xiaoR on 2021/1/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
